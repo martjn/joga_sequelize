fc62d1e71639c0d8f62b5aafde18c98c461334d8
martjn <mingdev77@gmail.com>
2024-01-27 18:29:11 +0200

    Getted all articles data

---
+++ controllers/article.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  "mysql://root:" +
+    encodeURIComponent("Ij6lS*H?ScwD") +
+    "@localhost:3306/joga_sequelize"
+);
+
+// read model data for table representation
+const Article = require("../models/article")(sequelize, Sequelize.DataTypes);
+
+// get all data from table
+const getAllArticles = (req, res) => {
+  const articles = Article.findAll().then(articles => {
+    console.log(articles)
+    return res.status(200).json({articles})
+  })
+  .catch(error => {
+    return res.status(500).send(error.message);
+  })
+
+}
+
+// export controller functions
+module.exports = {
+  getAllArticles
+}
--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 const express = require("express");
 const app = express();
-const {Sequelize} = require("sequelize")
 
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 // connect to database
 
-const sequelize = new Sequelize("mysql://root:" + encodeURIComponent("Ij6lS*H?ScwD") + "@localhost:3306/joga_sequelize");
-
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  "mysql://root:" +
+    encodeURIComponent("Ij6lS*H?ScwD") +
+    "@localhost:3306/joga_sequelize"
+);
 
 // testing connection
-sequelize.authenticate().then(() => {
-  console.log("Connected to the database")
-})
-.catch(err => {
-  console.error("Unable to connect to the database:", err);
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-app.get('/', (req, res) => {
-  res.json({message:"welcome to sequelize application"})
-})
+//using routes and controllers
+const articleRouter = require("./routes/article");
+app.use("/", articleRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to sequelize application" });
+});
 
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`)
-})
+  console.log(`server is running on http://localhost:${PORT}`);
+});
