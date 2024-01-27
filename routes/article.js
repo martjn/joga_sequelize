63b20428c6f3c6ee26dcb8c800d40b427eb11f92
martjn <mingdev77@gmail.com>
2024-01-27 20:59:26 +0200

    Delete article data

--- routes/article.js
+++ routes/article.js
@@ -5,8 +5,8 @@ const articleAdminController = require("../controllers/admin/article")
 
 router.get("/", articleController.getAllArticles);
 router.get("/article/:slug", articleController.getArticleBySlug);
-
 router.post('/admin/article/create', articleAdminController.createArticle);
-router.put('/admin/article/edit/:id', articleAdminController.editArticle)
+router.all('/admin/article/edit/:id', articleAdminController.editArticle);
+router.post('/admin/article/edit/:id', articleAdminController.deleteArticle);
 
 module.exports = router;
